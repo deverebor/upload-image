6f7bbdc0aa7882354cce15c9f5a992074576b6df
Lucas Souza <lucasp.sdev@gmail.com>
2021-11-11 08:49:34 -0300

    feat: solve last erros

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,75 @@
-import { useFavoriteImages } from '../contexts/FavoriteImagesContext';
-import { useImageAlbum } from '../contexts/ImageAlbumContext';
-import { Album } from '../components/Album';
+import { Button, Box } from '@chakra-ui/react';
+import { useMemo } from 'react';
+import { useInfiniteQuery } from 'react-query';
+
+import { Header } from '../components/Header';
+import { CardList } from '../components/CardList';
+import { api } from '../services/api';
+import { Loading } from '../components/Loading';
+import { Error } from '../components/Error';
+
+interface Image {
+  title: string;
+  description: string;
+  url: string;
+  ts: number;
+  id: string;
+}
+
+interface GetImagesResponse {
+  after: string;
+  data: Image[];
+}
 
 export default function Home(): JSX.Element {
+  async function fetchImages({ pageParam = null }): Promise<GetImagesResponse> {
+    const { data } = await api('/api/images', {
+      params: {
+        after: pageParam,
+      },
+    });
+    return data;
+  }
+
   const {
-    formattedData: favoriteFormattedData,
-    hasNextPage: favoriteHasNextPage,
-    isLoading: favoriteIsLoading,
-    isError: favoriteIsError,
-    fetchNextPage: favoriteFetchNextPage,
-    showFavorites } = useFavoriteImages();
-  const {
-    formattedData,
-    hasNextPage,
+    data,
     isLoading,
     isError,
-    fetchNextPage } = useImageAlbum();
+    isFetchingNextPage,
+    fetchNextPage,
+    hasNextPage,
+  } = useInfiniteQuery('images', fetchImages, {
+    getNextPageParam: lastPage => lastPage?.after || null,
+  });
+
+  const formattedData = useMemo(() => {
+    const formatted = data?.pages.flatMap(imageData => {
+      return imageData.data.flat();
+    });
+    return formatted;
+  }, [data]);
+
+  if (isLoading && !isError) {
+    return <Loading />;
+  }
+
+  if (!isLoading && isError) {
+    return <Error />;
+  }
 
   return (
     <>
-      <Album
-        isLoading={showFavorites ? favoriteIsLoading : isLoading}
-        isError={showFavorites ? favoriteIsError : isError}
-        formattedData={showFavorites ? favoriteFormattedData : formattedData}
-        hasNextPage={showFavorites ? favoriteHasNextPage : hasNextPage}
-        fetchNextPage={showFavorites ? favoriteFetchNextPage : fetchNextPage}
-      />
+      <Header />
+
+      <Box maxW={1120} px={[10, 15, 20]} mx="auto" my={[10, 15, 20]}>
+        <CardList cards={formattedData} />
+
+        {hasNextPage && (
+          <Button onClick={() => fetchNextPage()} disabled={isFetchingNextPage}>
+            {isFetchingNextPage ? 'Carregando...' : 'Carregar mais'}
+          </Button>
+        )}
+      </Box>
     </>
   );
 }
